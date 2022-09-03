991190d09ad8a1f3708a254ecda2613a5b614ec0
Sarthak Pawar <85248375+Grumppie@users.noreply.github.com>
2022-09-03 11:58:28 +0530

    script added

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import cross_origin
+from spellchecker import SpellChecker
+import json
 import os
 
 app = Flask(__name__)
 
+spell = SpellChecker()
 
-@app.route('/')
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    if request.method == 'GET':
+        return 'hi'
+    word = json.loads(request.data)['Log']
+    response = jsonify(correct=spell.correction(word))
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
+ 
